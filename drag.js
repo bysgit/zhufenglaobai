2b4a5412882726361b2caf685f1f3d05edc2e4f6
bysgit <83872385@qq.com>
2015-10-22 22:30:30 +0800

    change3 drag.js and demo1.html

--- drag.js
+++ drag.js
@@ -1,6 +1,9 @@
 /**
  * Created by asp on 2015/10/22.
  */
-function drag(){
-    
+function drag(id){
+    var obj=document.getElementById(id);
+    var disX=0;
+    var disY=0;
+    obj.onmousedown=function(){};
 }
